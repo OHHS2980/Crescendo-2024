0b49a75b0fd1bf8e6cd1d5b58d96939830e8cebc
OHHS2980 <ohhs2980@gmail.com>
2024-02-06 22:00:31 -0800

    ceated climber classes

---
+++ src/main/java/frc/robot/commands/ClimberCmd.java
@@ -0,0 +1,55 @@
+package frc.robot.commands;
+
+import java.util.function.BooleanSupplier;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ClimberSubsystem;
+
+public class ClimberCmd extends Command{
+    private final ClimberSubsystem climberSubsystem;
+
+    private BooleanSupplier extend;
+    private BooleanSupplier retract;
+
+
+    public ClimberCmd(ClimberSubsystem climberSubsystem,
+        BooleanSupplier extend,
+        BooleanSupplier retract) {
+
+        this.extend = extend;
+        this.retract = retract;
+
+        this.climberSubsystem = climberSubsystem;
+        addRequirements(climberSubsystem);
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+    
+    @Override
+    public void execute() {
+        if(extend.getAsBoolean()){ 
+            climberSubsystem.setClimberVoltage(1.0);
+        }else if (retract.getAsBoolean()){
+            climberSubsystem.setClimberVoltage(-1.0);
+        }else{
+            climberSubsystem.setClimberVoltage(0.0);
+        }
+
+        SmartDashboard.putNumber("LeftClimberPos", climberSubsystem.getLeftClimberPos());
+        SmartDashboard.putNumber("RightClimberPos", climberSubsystem.getRightClimberPos());
+    }
+    
+    @Override
+    public void end(boolean interrupted) {
+        
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -0,0 +1,62 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ClimberConstants;
+
+public class ClimberSubsystem extends SubsystemBase{
+    private final CANSparkMax leftMotor = new CANSparkMax(ClimberConstants.climberLeftID, MotorType.kBrushless);
+    private final CANSparkMax rightMotor = new CANSparkMax(ClimberConstants.climberRightID, MotorType.kBrushless);
+
+    public ClimberSubsystem(){
+        leftMotor.getEncoder().setPositionConversionFactor(ClimberConstants.positionConversionFactor);
+        rightMotor.getEncoder().setPositionConversionFactor(ClimberConstants.positionConversionFactor);
+
+        setClimberValues(ClimberConstants.climberP, ClimberConstants.climberI, ClimberConstants.climberD);
+    }
+    
+
+    public void home(){
+        setClimberPos(ClimberConstants.stowedEncoderVal, ClimberConstants.stowedEncoderVal);
+    }
+
+    public void maxHeight(){
+        setClimberPos(ClimberConstants.maxEncoderVal, ClimberConstants.maxEncoderVal);
+    }
+
+
+
+    /*       utilities       */
+
+
+
+    public void setClimberValues(double p, double i, double d){
+        leftMotor.getPIDController().setP(p);
+        leftMotor.getPIDController().setP(i);
+        leftMotor.getPIDController().setP(d);
+        
+        rightMotor.getPIDController().setP(p);
+        rightMotor.getPIDController().setP(i);
+        rightMotor.getPIDController().setP(d);
+    }
+    
+    public void setClimberVoltage(double power){
+        leftMotor.set(power);
+        rightMotor.set(power);
+    }
+
+    public void setClimberPos(double leftPos, double rightPos){
+        leftMotor.getPIDController().setReference(leftPos, CANSparkMax.ControlType.kSmartMotion);
+        rightMotor.getPIDController().setReference(rightPos, CANSparkMax.ControlType.kSmartMotion);
+    }
+
+    public double getLeftClimberPos(){//inches
+        return leftMotor.getEncoder().getPosition();
+    }
+
+    public double getRightClimberPos(){//inches
+        return rightMotor.getEncoder().getPosition();
+    }
+}
